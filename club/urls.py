8b7eecc40afbc4074c9b27edb0967a3754f7fd20
krayzie <edwinh300@gmail.com>
2022-02-14 14:42:44 -0800

    added meeting detail views page

--- club/urls.py
+++ club/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('resources/', views.getResources, name='resources'),
+    path('meetings/', views.getMeetings, name='meetings'),
+    path('meetingDetails/<int:id>', views.getMeetingDetails, name='meetingDetails'),
 ]
--- club/views.py
+++ club/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Meeting, MeetingMinutes, Resource, Event
+from django.shortcuts import  get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,16 @@ def index(request):
 def getResources(request):
     resource_list = Resource.objects.all()
     return render(request, 'club/resources.html' ,{'resource_list' : resource_list})
+
+def getMeetings(request):
+    meetings_list = Meeting.objects.all()
+    return render(request, 'club/meetings.html', {'meetings_list': meetings_list})
+
+def getMeetingDetails(request, id):
+    meet = get_object_or_404(Meeting, pk=id)
+    meetDetails = meet.meetingdescription
+    context = {
+        'meet' : meet,
+        'meetDetails' : meetDetails,
+    }
+    return render(request, 'club/meetingDetails.html', context=context)
